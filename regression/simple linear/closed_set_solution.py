3217947f867150cec0f42aeebcb5b0bf946cf372
Glissando <justinjoelward@gmail.com>
2018-04-06 02:18:10 -0500

    Automatically set bounds

--- regression/simple linear/closed_set_solution.py
+++ regression/simple linear/closed_set_solution.py
@@ -2,8 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = [4.87*(10**-6), 5.4*(10**-6), 6.06*(10**-6), 6.89*(10**-6), 8*(10**-6), 9.52*(10**-6)]
-y = [100.3, 111.6, 124.3, 139.8, 158.2, 174.9]
+x = [3.03*(10**-2), 1*(10**-3), 2.2*(10**-2), 1*(10**-2), 4.5*(10**-2), 8.3*(10**-2)]
+y = [1.6*(10**-3), 6.1*(10**-3), 26.1*(10**-3), 56.7*(10**-3), 0.23, 0.30]
 n = len(x)
 
 #helper functions
@@ -77,6 +77,7 @@ def DrawGraph():
     xline.append(x[len(x)-1])
     yline.append(slope_intercept_form(slope * x[len(x)-1], intercept))
     print(slope*x[len(x)-1])
+    print(slope)
     #Debugging
     #for vx in range(len(x)):
         #vy = slope * vx + intercept
@@ -89,11 +90,16 @@ def DrawGraph():
     #Plot the closed solution
     plt.plot(xline, yline)
     #Boilerplate
-    plt.xlabel('Inverse Volume')
-    plt.ylabel('Pressure')
-    plt.title('Graph of Pressure and Inverse Volume')
+    plt.xlabel('Inverse resistance')
+    plt.ylabel('Current')
+    plt.title('Graph of Current and Inverse Resistance')
     plt.savefig('pv_diagram.png')
-    #plt.xlim([0,0.00001])
+    xLowerBound = x[x.index(min(x))]
+    xUpperBound = x[x.index(max(x))]
+    yLowerBound = y[y.index(min(y))]
+    yUpperBound = y[y.index(max(y))]
+    plt.xlim([xLowerBound - 0.1*xLowerBound, xUpperBound + 0.1*xUpperBound])
+    plt.ylim([yLowerBound - 0.1*yLowerBound, yUpperBound + 0.1*yUpperBound])
     plt.show()
 
 DrawGraph()
